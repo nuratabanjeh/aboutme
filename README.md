dc3b89ec1b884d4133be2df81ebb0319bfd02717
nura tabanjeh <nuratabanjeh96@gmail.com>
2021-04-06 17:29:35 +0300

    gussgame

--- README.md
+++ README.md
@@ -1,12 +1,8 @@
-## How did this go, overall?
+# LAB Number 03
 
-**very good**
+## Project Name: About Me
 
+i Build this regarding to the required on cancas in order to make a good website
 
-## What observations or questions do you have about what you’ve learned so far?
+Author: Nura Tabanjeh.
 
-**i have no questions, every thing going well**
-
-## How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
-
-**two hours and a half untile now, i thought it will take one hour, but it's ok becuase i took so long in the CSS part**
--- js/app.js
+++ js/app.js
@@ -3,76 +3,114 @@ let userName = prompt('welcome to my website, what is your name?');
 userName = userName.toLowerCase();
 //console.log(userName);
 alert('Hello '+ userName);
-
+let score =0;
 let job = prompt('do you think i am an engineer?');
 switch(job.toLowerCase()){
 case'yes':
 case'y':
-alert('Bravo '+ userName);
-break;
+  alert('Bravo '+ userName);
+  score++;
+  break;
 case'no':
 case'n':
-    alert('wrong');
-    break;
-    default:
-        alert('its ok');
+  alert('wrong');
+  break;
+default:
+  alert('its ok');
 }
 let freeTime = prompt('do you think i like to spend time with my friends?');
 switch(freeTime.toLowerCase()){
-    case'yes':
-    case'y':
-    alert('ofcourse ' + userName);
-    break;
-    case'no':
-    case'n':
-    alert('wrong');
-    break;
-    default:
-        alert('its ok');
+case'yes':
+case'y':
+  alert('ofcourse ' + userName);
+  score++;
+  break;
+case'no':
+case'n':
+  alert('wrong');
+  break;
+default:
+  alert('its ok');
 }
 
 let myfriend = prompt('would you like to be my friend?');
 switch(myfriend.toLowerCase()){
-    case'yes':
-    case'y':
-    alert('nice to meet you my friend');
-    break;
-    case'no':
-    case'n':
-    alert('you loser');
-    break;
-    default:
-        alert('its ok');
+case'yes':
+case'y':
+  alert('nice to meet you my friend');
+  score++;
+  break;
+case'no':
+case'n':
+  alert('you loser');
+  break;
+default:
+  alert('its ok');
 }
 let music = prompt('do you think i like classic music?');
 switch(music.toLowerCase()){
-    case'yes':
-    case'y':
-    alert('yes i really enjoy this type of music');
-    break;
-    case'no':
-    case'n':
-    alert(' OMG wrong');
-    break;
-    default:
-        alert('its ok');
+case'yes':
+case'y':
+  alert('yes i really enjoy this type of music');
+  score++;
+  break;
+case'no':
+case'n':
+  alert(' OMG wrong');
+  break;
+default:
+  alert('its ok');
 }
 
 let series = prompt('do you think i like historical series?');
 switch(series.toLowerCase()){
-    case'yes':
-    case'y':
-    alert('yes i really enjoy them specially "vikings"');
+case'yes':
+case'y':
+  alert('yes i really enjoy them specially "vikings"');
+  score++;
+  break;
+case'no':
+case'n':
+  alert('wrong');
+  break;
+default:
+  alert('its ok');
+}
+let myage = parseInt(prompt('guess my age'));
+for(let i=1; i<=4; i++){
+  if(myage === 25){
+    alert('right');
+    score++;
     break;
-    case'no':
-    case'n':
-    alert('wrong');
+  } else if(myage>25){
+
+    console.log(myage);
+
+    myage=parseInt(prompt('too high try again please'));
+  } else if(myage<25){
+
+    myage=parseInt(prompt('too low try again please'));
+  }
+}
+let topsong=['iris','yellow','grace kelly'];
+let usersong = prompt('guess my favorite song');
+for(let i=1; i<=6; i++){
+    
+  if(usersong === topsong[0]||usersong=== topsong[1]||usersong===topsong[2]){
+    alert('correct');
+    score++;
     break;
-    default:
-        alert('its ok');
+  }else {
+    usersong = prompt('wrong guess again please');
+  }
+
+
 }
 
-alert('No more questions ' + userName +' enjoy my website');
+
+
+
+alert('No more questions ' + userName + 'your score is ' + score + ' enjoy my website');
 
 
 
